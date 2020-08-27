dd19b1922e5804cc29216dd0e56eb01a0df3b670
thisRandomName <49452494+thisRandomName@users.noreply.github.com>
2020-08-27 05:47:23 +0300

    Update hooray.js

--- cutestCodeEver/hooray.js
+++ cutestCodeEver/hooray.js
@@ -62,7 +62,7 @@ function render(){
         // Adjust the velocities:
         confet.veloc.x -= confet.veloc.x * drag; //x-axis slow-down
         confet.veloc.y = Math.min(confet.veloc.y + gravity, velocity);//y-axis velocity
-        if(confet.veloc.x += Math.random()>0.5){confet.veloc.x-=Math.random();};//some will move a littile bit to the left
+        if(confet.veloc.x += Math.random()>0.5){confet.veloc.x-=Math.random();};//some will move a little bit to the left
 
         // Set  each confeti's position:
         confet.position.x += confet.veloc.x;
